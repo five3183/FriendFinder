b43cfeab8216a23c905115fa932c89e3306d1103
Matthew Curran <five3183@gmail.com>
2018-11-08 16:14:05 -0500

    updated json file

--- app/data/friends.js
+++ app/data/friends.js
@@ -12,7 +12,77 @@ var friendArray = [
     Q7: 1,
     Q8: 1,
     Q9: 5
-    }
+    }, 
+   {
+   name: "Harley Quinn",
+   image: "https://media.giphy.com/media/tuo6Xr0xRAepG/giphy.gif",
+   Q0: 5,
+   Q1: 1,
+   Q2: 4,
+   Q3: 1,
+   Q4: 5,
+   Q5: 1,
+   Q6: 2,
+   Q7: 1,
+   Q8: 4,
+   Q9: 4
+   },
+   {
+   name: "Donald Trump",
+   image: "https://media.giphy.com/media/xTiTnHXbRoaZ1B1Mo8/giphy.gif",
+   Q0: 2,
+   Q1: 2,
+   Q2: 2,
+   Q3: 2,
+   Q4: 1,
+   Q5: 5,
+   Q6: 1,
+   Q7: 2,
+   Q8: 1,
+   Q9: 3
+   },
+   {
+   name: "Oscar the Grouch",
+   image: "https://media.giphy.com/media/oBJ3iITOA7mBG/giphy.gif",
+   Q0: 1,
+   Q1: 5,
+   Q2: 4,
+   Q3: 1,
+   Q4: 5,
+   Q5: 5,
+   Q6: 1,
+   Q7: 5,
+   Q8: 1,
+   Q9: 1
+   },
+   {
+   name: "Cheech",
+   image: "https://media.giphy.com/media/4GRRBtKrdiFDa/giphy.gif",
+   Q0: 4,
+   Q1: 2,
+   Q2: 4,
+   Q3: 2,
+   Q4: 4,
+   Q5: 2,
+   Q6: 3,
+   Q7: 2,
+   Q8: 3,
+   Q9: 5
+   }, 
+   {
+   name: "Clair Underwood",
+   image: "https://media.giphy.com/media/1kybCHYs7NGCY/giphy.gif",
+   Q0: 5,
+   Q1: 5,
+   Q2: 4,
+   Q3: 2,
+   Q4: 4,
+   Q5: 2,
+   Q6: 4,
+   Q7: 4,
+   Q8: 2,
+   Q9: 4
+   }
 ]
 
 module.exports = friendArray
